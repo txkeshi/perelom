459c5be281973f8ca4432374bd38886eb349b1ae
Raul Knyazev <117115184+txkeshi@users.noreply.github.com>
2023-01-06 02:42:37 +0300

    Update sv_perelom.lua

--- sv_perelom.lua
+++ sv_perelom.lua
@@ -1,10 +1,6 @@
 -- Получение перелома от падение 
-local weap = {['rust_al47'] = true}
-local chance
 hook.Add('EntityTakeDamage', 'perelom', function(target, dmginfo)
     if (target:IsPlayer() and dmginfo:IsFallDamage()) then
-        chance = math.random(1, 3) -- сам выбираешь(33% now)
-        if chance == 3 then
             dmginfo:SetDamage(15)
             target:SetRunSpeed(65)
             target:SetWalkSpeed(65)
