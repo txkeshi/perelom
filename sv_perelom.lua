d46f1ff9c691dd68a536db6c74ee3fd9646863a2
Raul Knyazev <117115184+txkeshi@users.noreply.github.com>
2023-01-06 02:52:41 +0300

    Fixed v2
    Fixed v2

--- sv_perelom.lua
+++ sv_perelom.lua
@@ -1,9 +1,9 @@
 -- Получение перелома от падение 
 hook.Add('PlayerTakeDamage', 'perelom', function(target, dmginfo)
-    if (target:IsPlayer() and dmginfo:IsFallDamage()) then
+    if (target:IsPlayer() and dmginfo:PlayerFallDamage()) then
             dmginfo:SetDamage(15)
             target:SetRunSpeed(65)
             target:SetWalkSpeed(65)
             target:SetJumpPower(65)
             target:ChatPrint('Вы Получили Перелом')
-end
+end)
